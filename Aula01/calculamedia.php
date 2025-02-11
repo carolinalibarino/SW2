53f3d489e562ac81bf6b59502ce2bb24517e3030
Carolina Libarino <124791376+carolinalibarino@users.noreply.github.com>
2025-02-11 12:41:52 -0300

    Add files via upload

---
+++ Aula01/calculamedia.php
@@ -0,0 +1,16 @@
+<?php
+ $nota1 = 8;
+ $nota2 = 6;
+ $nota3 = 8;
+ $notafinal = ($nota1 + $nota2 + $nota3)/3;
+
+ if($notafinal >=7){
+    echo "Média = $notafinal , APROVADO";
+ }
+
+     else {
+            echo "REPROVADO";
+     }
+
+
+?>
