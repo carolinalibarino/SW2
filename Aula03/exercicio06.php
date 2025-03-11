f04c4bb330a3d7111e4592e1574186b708b4d259
carolinalibarino <carollibarino2@gmail.com>
2025-03-11 08:34:19 -0300

    aula 03

--- Aula03/exercicio06.php
+++ Aula03/exercicio06.php
@@ -1,3 +1,14 @@
 <?php
 
+function geradornum(){
+    $vetor = array();
+
+    for ($i=0; $i < 9; $i++) { 
+        $vetor[$i]= rand (0,100);
+    }
+    return $vetor;
+}
+
+$recebe = geradornum();
+print_r($recebe);
 ?>
