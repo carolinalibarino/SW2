5194895951ef9f55c4552821123d7e46ba7839a2
carolinalibarino <carollibarino2@gmail.com>
2025-04-22 07:58:00 -0300

    aula 22 de abril

--- Aula06/api.php
+++ Aula06/api.php
@@ -1,7 +1,7 @@
 <?php
 
 //cabeçalho
-header("Content-Type: application/json"); //tipo de resposta json
+header("Content-Type: application/json; charset=UTF-8"); //tipo de resposta json
 $metodo = $_SERVER["REQUEST_METHOD"]; 
 
 
@@ -24,23 +24,51 @@ $usuarios = json_decode(file_get_contents($arquivo), true); //pega o conteudo do
 switch ($metodo) {
     case 'GET':
         //echo "AQUI É O MÉTODO GET";
-        echo json_encode($usuarios); //converte para json e retorna
+/*         echo json_encode($usuarios); //converte para json e retorna
+        break; */
+        if (isset($_GET['id'])){
+            //se o id for passado, procura o usuario
+            $id = intval($_GET['id']);
+           $usuario_encontrado =  null;
+
+            //procura o usuario no array
+            foreach ($usuarios as $usuario) {
+                if ($usuario['id'] == $id) {
+                    $usuario_encontrado = $usuario;
+                    break;
+                }
+            }
+            if ($usuario_encontrado) {
+                echo json_encode($usuario_encontrado, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+            } else {
+                http_response_code(404); //código de erro 404
+                echo json_encode(["erro" => "USUÁRIO NÃO ENCONTRADO"], JSON_UNESCAPED_UNICODE);
+            }
+        }else{
+            //se o id não for passado, retorna todos os usuarios
+            echo json_encode($usuarios, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE); //converte para json e retorna
+        }
         break;
 
     case 'POST':
         //echo "AQUI É O MÉTODO POST";
-        $dados = json_decode(file_get_contents("php://input"), true); //pega os dados enviados
+        $dados = json_decode(file_get_contents('php://input'), true); //pega os dados enviados
         
         
-        if (!isset($dados["id"]) || !isset($dados["nome"]) || !isset($dados["email"])){
+        if (!isset($dados["nome"]) || !isset($dados["email"])){
             http_response_code(400); //código de erro 400
             echo json_encode(["erro" => "DADOS IMCOMPLETOS"], JSON_UNESCAPED_UNICODE);
             exit;
         }
         
+        $novo_id = 1;
+        if (!empty($usuarios)) {
+           $ids = array_column($usuarios, 'id'); //pega os ids dos usuarios
+           $novo_id = max($ids) + 1; //pega o maior id e soma 1
+        }
         //print_r($dados);
-        $novousuario =[
-            "id" => $dados["id"],
+        $novo_usuario =[
+            "id" => $novo_id,
             "nome" => $dados["nome"],
             "email" => $dados["email"]
         ];
@@ -52,7 +80,9 @@ switch ($metodo) {
         file_put_contents($arquivo, json_encode($usuarios, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)); 
 
         //retorna mensagem
-        echo json_encode(["mensagem" => "Usuário adicionado com sucesso!", "usuarios" => $usuarios], JSON_UNESCAPED_UNICODE);
+        echo json_encode([
+            "mensagem" => "Usuário adicionado com sucesso!", 
+            "usuarios" => $usuarios], JSON_UNESCAPED_UNICODE);
 
         /* //ADICIONA AO ARRAY
         array_push($usuarios, $novousuario);
